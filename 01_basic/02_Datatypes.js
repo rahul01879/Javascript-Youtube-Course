ca83c672bf5e9208c85d4bd2714d3d11b8d3cd45
rahul01879 <rahul001879@gmail.com>
2026-04-18 07:46:24 +0000

    dataTypes

---
+++ 01_basic/02_Datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // this use it at the start of code for new Javascript
+
+// Datatype Example
+
+let name = "Rahul" // string
+let age = 43 // number
+let isLogined = true  //boolean
+let state; // undefined
+
+// Datatypes
+
+// number = 2 to power 52
+// string = ""
+// bigInt
+// boolean = true or false
+// symbol = it is mostly use in react figma for uniqueness
+// null = standalone value
+// undefined = when variable value is not defined ex. let state;
+
+
+// typeOf
+
+console.log(typeof null)
+
+
